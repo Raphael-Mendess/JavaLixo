1f23a39fd1c0644de0071965a4bfaf6de38271e9
Raphael-Mendess <raphael.hkiller@gmail.com>
2025-12-16 11:11:48 -0300

    Department entity class

--- db/DB.java
+++ db/DB.java
@@ -9,13 +9,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-
 public class DB {
     private static Connection conn = null;
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         if (conn == null) {
-           try {          
+            try {
                 Properties props = loadProperties();
                 String url = props.getProperty("dburl");
                 conn = DriverManager.getConnection(url, props);
@@ -46,23 +45,23 @@ public class DB {
         }
     }
 
-   public static void closeStatement(Statement st){
-       if(st != null){
-          try {
-              st.close();
-          } catch (SQLException e) {
-             throw new DBException(e.getMessage());
-          }
-       } 
-   } 
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
 
-      public static void closeResultSet(ResultSet rs){
-       if(rs != null){
-          try {
-              rs.close();
-          } catch (SQLException e) {
-             throw new DBException(e.getMessage());
-          }
-       } 
-   } 
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
 }
